ecace32c9475c7b22750e9a0398f5d3a8118e64d
jamtechdev <jamtechba@gmail.com>
2025-05-26 15:22:37 +0530

    change logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,32 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased text-gray-900 ">
-        <div class="flex items-center min-h-screen pt-6 bg-[#edf2fe] sm:justify-between  sm:pt-0 dark:bg-gray-900 ">
-            <div class="xl:w-[50%] md:w-[100%] w-[100%] ">
-                <a href="/" class="text-center">
-                    <x-application-logo class="w-[100%] max-w-[500px] mx-auto text-gray-500 fill-current" />
-                </a>
-            </div>
-            <div class="xl:w-[45%] md:w-[100%] w-[100%] h-screen bg-white flex-col p-[15px] flex items-center justify-center">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-                <div class="px-6 py-4 mt-6 w-[100%] max-w-[650px] overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
-                    {{ $slot }}
-                </div>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased text-gray-900 ">
+    <div
+        class="flex items-center min-h-screen pt-6 bg-gradient-to-r from-blue-600 to-blue-800 sm:justify-between sm:pt-0 ">
+        <div class="xl:w-[50%] md:w-[100%] w-[100%] ">
+            <a href="/" class="text-center">
+                <x-application-logo class="w-[100%] max-w-[350px] mx-auto " />
+
+                <h1 class="mb-2 text-4xl font-bold text-center text-white">{{ config('app.name', 'LearnEdge') }}</h1>
+                <p class="font-bold text-white">Empowering education through smart learning tools, tests, and
+                    analytics.</p>
+            </a>
+        </div>
+        <div
+            class="xl:w-[45%] md:w-[100%] w-[100%] h-screen bg-white flex-col p-[15px] flex items-center justify-center">
+
+            <div
+                class="px-6 py-4 mt-6 w-[100%] max-w-[650px] overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+                {{ $slot }}
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
